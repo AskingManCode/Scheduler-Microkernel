e4ef1db764e315a7a174c449cedc1dfafaa6e571
AskingMansOz <sebasjimearrieta@gmail.com>
2026-07-03 15:03:06 -0600

    planificador.h finalizado

---
+++ include/planificador.h
@@ -0,0 +1,35 @@
+
+/**
+ * @file planificador_h
+ * 
+ * Round Robin = "Turno Rotativo"
+ * - Cada proceso recibe un "quantum" (porcion de tiempo) de CPU
+ * - Si no termina, se reubica al final de la cola
+ * - Se repite hasta que todos terminan.
+ */
+
+#ifndef PLANIFICADOR_H
+#define PLANIFICADOR_H
+
+
+// -------------------------------------------------------------
+// IMPORTACIONES
+// -------------------------------------------------------------
+
+#include "proceso.h"
+
+
+// -------------------------------------------------------------
+// CONSTANTES
+// -------------------------------------------------------------
+
+#define QUANTUM_DEFECTO 4 // 4 unidades de tiempo
+
+
+// -------------------------------------------------------------
+// FUNCIONES (Contratos)
+// -------------------------------------------------------------
+
+void planificador(PCB *tabla_PCB, int total_procesos, int quantum, long *esperas, long *respuestas, long *retornos, int pausa_ms);
+
+#endif // PLANIFICADOR_H
---
+++ src/planificador.c
@@ -0,0 +1,10 @@
+#include "planificador.h"
+
+/**
+ * @file planificador.c
+ *
+ * Responsable de decidir en todo momento que proceso
+ * de la cola de listos recibe la CPU, durante
+ * cuanto tiempo, y que ocurre cuando este tiempo se
+ * agota o cuando el proceso concluye
+ */
